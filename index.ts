7cb5506f41aa1ec672d1b092e3e2b19d37dc06e4
Rifat <rifat.islam173@northsouth.edu>
2024-06-16 00:08:59 +0600

    initial pulumi project setup

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { vpcAId } from "./vpc/vpcA";
+export { vpcA } from "./vpc/vpcA";
+export { vpcACidrBlock } from "./vpc/vpcA";
+export { vpcApublicSubnetId } from "./vpc/vpcA";
+export { vpcAprivateSubnetId } from "./vpc/vpcA";
+// export  peeringID = vpcPeeringConn.id;
