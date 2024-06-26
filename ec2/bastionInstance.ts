b99fc57693f26ab290c58f5fed947d2461d7d9d6
Rifat <rifat.islam173@northsouth.edu>
2024-06-27 02:48:05 +0600

    depends on

--- ec2/bastionInstance.ts
+++ ec2/bastionInstance.ts
@@ -1,14 +1,11 @@
 import * as aws from "@pulumi/aws";
 import * as dotenv from "dotenv";
-import {
-  vpcAId,
-  vpcApublicSubnetId
-} from "../vpc/vpcA";
+import { vpcA, vpcApublicSubnetId } from "../vpc/vpcA";
 
 dotenv.config();
 
 const bastionServerSG = new aws.ec2.SecurityGroup("ec2-bastion-sg", {
-  vpcId: vpcAId,
+  vpcId: vpcA.id,
   description: "Allowing SSH and HTTP only",
   ingress: [
     {
@@ -36,15 +33,21 @@ const ec2KeyPair = new aws.ec2.KeyPair("ec2-kp", {
   publicKey: process.env.PUBLIC_KEY!
 });
 
-const bastionInstance = new aws.ec2.Instance("ec2-bastion-instance", {
-  instanceType: "t2.micro",
-  ami: "ami-04b70fa74e45c3917", //Ubuntu, 24.04 LTS
-  keyName: ec2KeyPair.keyName,
-  vpcSecurityGroupIds: [bastionServerSG.id],
-  subnetId: vpcApublicSubnetId,
-  tags: {
-    Name: "ec2-bastion-instance"
+const bastionInstance = new aws.ec2.Instance(
+  "ec2-bastion-instance",
+  {
+    instanceType: "t2.micro",
+    ami: "ami-04b70fa74e45c3917", //Ubuntu, 24.04 LTS
+    keyName: ec2KeyPair.keyName,
+    vpcSecurityGroupIds: [bastionServerSG.id],
+    subnetId: vpcApublicSubnetId,
+    tags: {
+      Name: "ec2-bastion-instance"
+    }
+  },
+  {
+    dependsOn: [vpcA, bastionServerSG]
   }
-});
+);
 
 export const bastionInstanceArn = bastionInstance.arn;
