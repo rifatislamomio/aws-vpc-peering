9ad2aeeaf4fd3e4ee860258e23a6663a701fdf9f
Rifat <rifat.islam173@northsouth.edu>
2024-07-05 01:39:54 +0600

    export resource ids

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-export { vpcAId, vpcAprivateSubnetId, vpcApublicSubnetId } from "./vpc/vpcA";
-export { vpcBId, vpcBprivateSubnetId, vpcBpublicSubnetId } from "./vpc/vpcB";
-export { vpcPeeringId } from "./vpc/vpcPeering";
+export { vpcAId, vpcBId } from "./vpc";
 export { bastionInstanceArn } from "./ec2/bastionInstance";
 export { primaryInstanceId } from "./ec2/primaryInstance";
 export { workerInstanceId } from "./ec2/workerInstance";
